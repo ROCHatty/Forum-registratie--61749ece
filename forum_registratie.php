07be52e8d997b81c79c38a76311297c49422edeb
Aaron Jonk <2119311@talnet.nl>
2019-10-01 13:25:29 +0200

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,6 @@
+<form method="POST" action="registratie_handler.php">
+	<input type="text" name="username" placeholder="Gebruikersnaam"><br />
+	<input type="number" name="age" placeholder="Leeftijd" min="12" value="12"><br />
+	<input type="email" name="email" placeholder="Email"><br /><br />
+	<input type="submit" value="Registreer">
+</form>
---
+++ registratie_handler.php
@@ -0,0 +1,3 @@
+Gebruikersnaam -> <?php echo $_POST['username']; ?><br />
+Leeftijd -> <?php echo $_POST['age']; ?><br />
+Email -> <?php echo $_POST['email']; ?><br />
